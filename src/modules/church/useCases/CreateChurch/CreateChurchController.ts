e4df7451afc630694a90570bd3716fa09c069efc
Abner Ferreira de Andrade <abner.andrade10@gmail.com>
2023-09-25 10:16:25 -0300

    added upload church logo on method post

--- src/modules/church/useCases/CreateChurch/CreateChurchController.ts
+++ src/modules/church/useCases/CreateChurch/CreateChurchController.ts
@@ -2,18 +2,12 @@ import { Request, Response } from "express";
 import { container } from "tsyringe";
 import { CreateChurchUseCase } from "./CreateChurchUseCase";
 
-
 class CreateChurchController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const {
-      name,
-      address,
-      address_number,
-      city,
-      district,
-      state,
-      logo,
-    } = request.body;
+    const { name, address, address_number, city, district, state } =
+      request.body;
+
+    const logo = request.file.filename;
 
     const createChurchUseCase = container.resolve(CreateChurchUseCase);
 
--- src/shared/infra/http/routes/church.routes.ts
+++ src/shared/infra/http/routes/church.routes.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
 import { CreateChurchController } from "@modules/church/useCases/CreateChurch/CreateChurchController";
+import multer from "multer";
+import uploadConfig from "@config/upload";
 
 const churchRoutes = Router();
 
 const createChurchController = new CreateChurchController();
 
-churchRoutes.post("/", createChurchController.handle);
+const uploadLogo = multer(uploadConfig.upload("./tmp/church"));
+
+churchRoutes.post(
+  "/",
+  uploadLogo.single("logo"),
+  createChurchController.handle,
+);
 
 export { churchRoutes };
